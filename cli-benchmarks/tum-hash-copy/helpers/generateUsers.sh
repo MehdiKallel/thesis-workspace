129efe2a85959875701918a98b963068f70d0c2b
Mehdi <ge72zar@mytum.de>
2022-04-19 20:03:17 +0000

    deleted useless files

--- cli-benchmarks/tum-hash-copy/helpers/generateUsers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-numFiles=$(sed -n '/workers:/,/spec:/p' ../config.yaml | grep -oP '(?<=number: ).*')
-echo "$numFiles are needed for the benchmark"
-
-for ((i=0; i < $numFiles; i++))
-do
-	echo "generating users data for Worker $i"
-	curl "https://api.mockaroo.com/api/f3c00fe0?count=100&key=c6843060" > "user.json" --output "/home/ubuntu/HyperLedgerLab-2.0/caliper/benchmarks/tum-hash/users$i.json"
-done
